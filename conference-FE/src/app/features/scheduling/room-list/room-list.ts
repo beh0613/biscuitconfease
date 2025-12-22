1ac7999e659e576fad2f71145208bd9b218271bd
Alex wong <alexwongwyf@gmail.com>
2025-12-22 20:45:16 +0800

    sidebar

--- conference-FE/src/app/features/scheduling/room-list/room-list.ts
+++ conference-FE/src/app/features/scheduling/room-list/room-list.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SchedulingService, Room } from '../../../services/scheduling'; // Check this path!
+import { SchedulingService, Room } from '../../../services/scheduling'; 
 
 @Component({
   selector: 'app-room-list',
